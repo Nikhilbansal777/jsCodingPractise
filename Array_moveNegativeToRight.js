1518d8b9589f91b67c78c19ca8bee56f6d66f1d9
Nikhil Bansal <48425495+Nikhilbansal777@users.noreply.github.com>
2024-08-25 17:35:15 +0000

    more program

---
+++ Array_moveNegativeToRight.js
@@ -0,0 +1,16 @@
+let a = [1,2,-4,-2,2,4,6,-5,-9];
+
+let j=0;
+
+for(let i=0;i<a.length;i++) {
+    if(a[i]<=0){
+        if(i!==j){
+        let temp = a[i];
+        a[i] = a[j]
+        a[j] = temp;
+    }
+    j++;
+}
+}
+
+console.log(a);
