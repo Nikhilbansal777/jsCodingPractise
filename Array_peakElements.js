a7fc767eb98c8dca91211854a5e57c108f002925
Nikhil Bansal <nikhilbansal@mentissoft.com>
2025-04-25 10:14:15 +0530

    more code

--- Array_peakElements.js
+++ Array_peakElements.js
@@ -1,17 +1,16 @@
 let a = [20, 25, 24, 51, 52, 45, 70, 5];
 
 function peakElement() {
-    let b = []
-    for (let i = 1; i < a.length; i++) {
-        if (a[i] > a[i-1] && a[i] > a[i + 1]) {
-           b.push(a[i])
-        } 
-        
+  let b = [];
+  for (let i = 1; i < a.length - 1; i++) {
+    if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+      b.push(a[i]);
     }
-    if (b.length == 0) {
-        return "no peak element"
-    }
-    return b
+  }
+  if (b.length == 0) {
+    return "no peak element";
+  }
+  return b;
 }
 
 console.log(peakElement());
